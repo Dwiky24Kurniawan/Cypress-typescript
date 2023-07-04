228b1bba0ddb2ac451dece3adab7d932c0172bbd
Dwiky24Kurniawan <dwiky24kurniawan@gmail.com>
2023-07-04 20:53:02 +0700

    update specs

--- cypress/e2e/specs/3-checkout.cy.ts
+++ cypress/e2e/specs/3-checkout.cy.ts
@@ -1,11 +1,11 @@
 import {LoginPage} from "../pages/login_pages"
 import {DashboardPage} from "../pages/dashboard_pages"
+
 let loginPage = new LoginPage()
 let dashboardPage = new DashboardPage()
 const URL = 'https://www.saucedemo.com'
 
 describe('Test Suite Sauce Demo - Checkout', () => {
-
     it('TC_004 - Positive Test - Checkout', () => {
         loginPage.login(URL, 'standard_user', 'secret_sauce')
         loginPage.assertLogin()
